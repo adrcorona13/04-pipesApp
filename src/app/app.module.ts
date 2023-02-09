093f0dbccc4cc88b721ff6f3f284dfb7d77c0502
Adrian Pineda <adrian.pc13@hotmail.com>
2023-02-08 19:56:57 -0600

    agregar menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
-import { PrimeNGModule } from './prime-ng/prime-ng.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +10,7 @@ import { PrimeNGModule } from './prime-ng/prime-ng.module';
   ],
   imports: [
     BrowserModule,
-    PrimeNGModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
