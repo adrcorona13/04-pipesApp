b3088308c3c9826803d5a93569364a85c37be514
Adrian Pineda <adrian.pc13@hotmail.com>
2023-02-09 00:06:03 -0600

    upper lower title case

--- src/app/ventas/pages/basicos/basicos.component.ts
+++ src/app/ventas/pages/basicos/basicos.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class BasicosComponent {
 
+  nombreLower: string = 'adrian';
+  nombreUpper: string = 'ADRIAN';
+  nombreCompleto: string = 'adRiAn CorOna';
 }
