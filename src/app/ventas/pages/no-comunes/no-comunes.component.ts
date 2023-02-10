8d77a99357841fce40169fa756ab3c249f1c1df3
Adrian Pineda <adrian.pc13@hotmail.com>
2023-02-10 14:15:13 -0600

    async pipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval} from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -64,4 +65,13 @@ export class NoComunesComponent {
       vuela: false
     },
   ]
+
+  // async pipe
+  miObservable = interval(1000);
+
+  valorPromesa = new Promise( (resolve, reject) => {
+    setTimeout(()=>{
+      resolve('Tenemos data de la promesa')
+    }, 3500)
+  });
 }
