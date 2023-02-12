1bed2956fc8707a967192f61d55a8be450a67af5
Adrian Pineda <adrian.pc13@hotmail.com>
2023-02-11 22:10:01 -0600

    agregar table y pipe de ordenamiento

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,23 +1,27 @@
 import { NgModule } from '@angular/core';
 
-import {ButtonModule} from 'primeng/button';
-import {CardModule} from 'primeng/card';
-import {FieldsetModule} from 'primeng/fieldset';
-import {MenubarModule} from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
+import { MenubarModule } from 'primeng/menubar';
+import { TableModule } from "primeng/table";
+import { ToolbarModule } from "primeng/toolbar";
 
 
 @NgModule({
-    declarations:[
+    declarations: [
     ],
-    imports:[
+    imports: [
     ],
-    exports:[
+    exports: [
         ButtonModule,
-        CardModule, 
+        CardModule,
         FieldsetModule,
-        MenubarModule,
+        MenubarModule,  
+        ToolbarModule,
+        TableModule
     ]
 })
-export class PrimeNGModule{
+export class PrimeNGModule {
 
 }
--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class OrdenarComponent {
 
+  toggle: boolean = false;
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: 'Daredevil',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde
+    }
+  ]
+
+  toggleCase() {
+    this.toggle = !this.toggle;    
+  }
 }
